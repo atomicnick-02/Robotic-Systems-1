56829efd324c50d74b4fffc94c8ec0b16604121a
Nick Vemis <nickvm1202@gmail.com>
2025-04-20 17:08:59 +0300

    added camera but cant see the overlay. maybe the root node conversion is at fault

--- controllers/my_first_controller0/my_first_controller0.py
+++ controllers/my_first_controller0/my_first_controller0.py
@@ -1,31 +1,120 @@
+from controller import Robot, Camera, DistanceSensor, Motor
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+# Constants
+MAX_SENSOR_NUMBER = 16
+RANGE = 1024 / 2
 
-# create the Robot instance.
-robot = Robot()
+# Helper to clamp values
+def bound(x, a, b):
+    return a if x < a else b if x > b else x
 
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+def initialize(robot):
+    """
+    Initialize devices, sensors, motors, and Braitenberg weights.
+    Returns a dict with all handles and parameters.
+    """
+    # Basic timestep
+    time_step = int(robot.getBasicTimeStep())
+    # Robot name
+    robot_name = robot.getName()
 
-# Main loop:
-# - perform simulation steps until Webots is stopping the controller
-while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
+    # Default parameters
+    camera_enabled = True
+    range_val = RANGE
+    max_speed = 1.0
+    speed_unit = 1.0
 
-    # Process sensor data here.
+    # Weight matrices for various robots
+    
+    pioneer2_matrix = [
+        [-1, 15], [-3, 13], [-3, 8], [-2, 7],
+        [-3, -4], [-4, -2], [-3, -2], [-1, -1],
+        [-1, -1], [-2, -3], [-2, -4], [4, -3],
+        [7, -5], [7, -3], [10, -2], [11, -1]
+    ]
+    
 
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    # Determine robot type and parameters
 
-# Enter here exit cleanup code.
+    if robot_name == "Pioneer 2":
+        num_sensors = 16
+        sensor_prefix = "ds"
+        weights = pioneer2_matrix
+        max_speed = 10.0
+        speed_unit = 0.3
+    else:
+        print("This controller doesn't support robot:", robot_name)
+        exit(1)
+
+    # Initialize sensors
+    sensors = []
+    for i in range(num_sensors):
+        name = f"{sensor_prefix}{i}"
+        ds = robot.getDevice(name)
+        ds.enable(time_step)
+        sensors.append(ds)
+
+    # Initialize motors
+    left_motor = robot.getDevice("left wheel motor")
+    right_motor = robot.getDevice("right wheel motor")
+    left_motor.setPosition(float('inf'))
+    right_motor.setPosition(float('inf'))
+    left_motor.setVelocity(0.0)
+    right_motor.setVelocity(0.0)
+
+    # Initialize camera if present
+    cam = None
+    if camera_enabled:
+        cam = robot.getDevice("camera")
+        cam.enable(time_step)
+        cam.recognitionEnable(time_step)
+        print(f"Camera enabled with FOV: {cam.getFov()} rad")
+
+
+    print(f"Initialized {robot_name} with {num_sensors} sensors.")
+
+    return {
+        'time_step': time_step,
+        'sensors': sensors,
+        'weights': weights,
+        'range': range_val,
+        'max_speed': max_speed,
+        'speed_unit': speed_unit,
+        'left_motor': left_motor,
+        'right_motor': right_motor,
+        'camera': cam
+    }
+
+
+def run():
+    robot = Robot()
+    ctx = initialize(robot)
+
+    while robot.step(ctx['time_step']) != -1:
+        # Refresh camera image
+        if ctx['camera']:
+            ctx['camera'].getImage()
+
+        # Read sensor values
+        readings = [ds.getValue() for ds in ctx['sensors']]
+
+        # Braitenberg: compute wheel speeds
+        speed_l = 0.0
+        speed_r = 0.0
+        for i, val in enumerate(readings):
+            factor = 1.0 - (val / ctx['range'])
+            speed_l += ctx['speed_unit'] * ctx['weights'][i][0] * factor
+            speed_r += ctx['speed_unit'] * ctx['weights'][i][1] * factor
+
+        # Clamp
+        speed_l = bound(speed_l, -ctx['max_speed'], ctx['max_speed'])
+        speed_r = bound(speed_r, -ctx['max_speed'], ctx['max_speed'])
+
+        # Set velocities
+        ctx['left_motor'].setVelocity(speed_l)
+        ctx['right_motor'].setVelocity(speed_r)
+
+
+if __name__ == "__main__":
+    run()
